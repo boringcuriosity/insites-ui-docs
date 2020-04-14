be877804d99b4920863de0d0296e363d829f129d
Tomek Marciniak <tmarciniakm@gmail.com>
2020-04-14 21:37:59 +0200

    feat(input): add input

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,9 @@ module.exports = {
             'components/button',
             'components/container',
             'components/tooltip'
+          ],
+          Form: [
+            'form/input'
           ]
         }
       }
